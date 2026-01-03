2d4d954c4ffd5c54a687997deceaf9402c6aa339
Jsmybzd <1632841417@qq.com>
2026-01-03 21:55:08 +0800

    Add files via upload

--- config.py
+++ config.py
@@ -1,12 +1,13 @@
-# config.py
 import os
 from dotenv import load_dotenv
 
 load_dotenv()  # 加载 .env 文件
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = (
-        "mssql+pyodbc://localhost\\SQLEXPRESS/执法监管业务线"
-        "?driver=ODBC+Driver+17+for+SQL+Server"
-        "&trusted_connection=yes"
+    # 数据库连接配置（兼容SQLAlchemy）
+    SQLALCHEMY_DATABASE_URI = os.getenv(
+        "DATABASE_URI",
+        "mssql+pyodbc://localhost\\SQLEXPRESS/NationalParkDB?driver=ODBC+Driver+17+for+SQL+Server&trusted_connection=yes"
     )
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_ECHO = False  # 调试时可设为True，打印SQL语句
